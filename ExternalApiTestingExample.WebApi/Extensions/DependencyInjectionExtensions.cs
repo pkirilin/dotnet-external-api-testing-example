b7f8c4698f2fe37cb4c8b073a9181b3f5c6e1437
Pavel Kirilin <kirilin.pav@gmail.com>
2022-04-14 14:25:56 +0300

    Fixed typo

--- ExternalApiTestingExample.WebApi/Extensions/DependencyInjectionExtensions.cs
+++ ExternalApiTestingExample.WebApi/Extensions/DependencyInjectionExtensions.cs
@@ -2,7 +2,7 @@ using ExternalApiTestingExample.WebApi.Integrations;
 
 namespace ExternalApiTestingExample.WebApi.Extensions;
 
-public static class DepencencyInjectionExtensions
+public static class DependencyInjectionExtensions
 {
     public static IHttpClientBuilder AddTodosClient(this IServiceCollection services)
     {
